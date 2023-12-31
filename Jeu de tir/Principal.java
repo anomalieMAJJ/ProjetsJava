d97a44130e7b255b2dca8ff255efbd3c2fb71df6
Jessica Majeur <147209573+anomalieMAJJ@users.noreply.github.com>
2023-12-30 19:53:53 -0500

    Update Principal.java

--- Jeu de tir/Principal.java
+++ Jeu de tir/Principal.java
@@ -1,11 +1,5 @@
 import javax.swing.*;
 /**
- *  * @authors Jessica Majeur et Jessica Portillo
- *  * Codes permanents : MAJJ 2651 9105 et PORJ 1656 8802
- *  * Courriel : ge991970@ens.uqam.ca et portillo.jessica@courrier.uqam.ca
- *  * Cours : INF2120-20
- *  * @version 2023-08-04
- *
  * Cette classe permet de jouer à une jeu de type ‘tower defence’.
  */
 public class Principal {
